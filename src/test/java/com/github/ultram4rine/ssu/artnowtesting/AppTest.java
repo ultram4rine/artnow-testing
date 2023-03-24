abf989e67d52d49b9eaec0ee458194e734dc3870
ultram4rine <ultramarine730@gmail.com>
2023-03-24 15:16:52 +0400

    fix groupid

--- src/test/java/com/github/ultram4rine/ssu/artnowtesting/AppTest.java
+++ src/test/java/com/github/ultram4rine/ssu/artnowtesting/AppTest.java
@@ -1,4 +1,4 @@
-package com.github.ultram4rine.ssu.artnow-testing;
+package com.github.ultram4rine.ssu.artnowtesting;
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,14 +7,12 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
