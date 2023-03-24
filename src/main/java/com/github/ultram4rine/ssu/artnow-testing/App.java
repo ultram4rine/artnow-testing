ca167bdb816b50b9e9b7c3debbe6b8a100f51af0
ultram4rine <ultramarine730@gmail.com>
2023-03-24 15:11:20 +0400

    init

---
+++ src/main/java/com/github/ultram4rine/ssu/artnow-testing/App.java
@@ -0,0 +1,13 @@
+package com.github.ultram4rine.ssu.artnow-testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/ultram4rine/ssu/artnow-testing/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.ultram4rine.ssu.artnow-testing;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
